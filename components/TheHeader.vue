40d8ee78ab6458d91855cc3360833a83cbd33262
Sharyz <onshariiit@gmail.com>
2022-05-17 09:12:15 +0500

    feat: create the-header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <NuxtLink to="/">
+      <TheLogo />
+    </NuxtLink>
+    <TheNavbar />
+    <div class="flex items-center space-x-4">
+      <NuxtLink to="/signin" class="py-2 px-4 text-lg font-semibold">
+        Sign in
+      </NuxtLink>
+      <NuxtLink
+        to="/signup"
+        class="rounded-lg bg-rose-400 py-2 px-4 text-lg font-semibold text-white"
+      >
+        Sign up
+      </NuxtLink>
+    </div>
+  </header>
+</template>
+
+<script>
+import TheNavbar from './TheNavbar.vue'
+export default {
+  name: 'TheHeader',
+  components: { TheNavbar },
+}
+</script>
+
+<style scoped>
+.header {
+  @apply flex items-center justify-between py-6 px-4;
+}
+</style>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <ul class="nav-list">
+      <li
+        v-for="navItem in navItems"
+        :key="navItem.label"
+        class="nav-list-item"
+      >
+        <NuxtLink :to="navItem.path">{{ navItem.label }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+  data() {
+    return {
+      navItems: [
+        { path: '/', label: 'Home' },
+        { path: '/explore', label: 'Explore' },
+        { path: '/resources', label: 'Resources' },
+        { path: '/contact', label: 'Contact' },
+        { path: '/blog', label: 'Blog' },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.nav-list {
+  @apply flex items-center space-x-12;
+}
+
+.nav-list-item {
+  @apply py-1 px-2 text-lg font-semibold;
+}
+
+.nav-list-item .nuxt-link-active {
+  @apply border-b-2 border-blue-900;
+}
+</style>
